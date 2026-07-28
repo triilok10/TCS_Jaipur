be4091dbbfac02eca348d2416c5c4ed43f2899f2
triilok10 <ititku3@gmail.com>
2026-07-28 17:42:14 +0530

    Working

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Hero } from '../hero/hero';
 import { About } from '../about/about';
+import { Connect } from '../connect/connect';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, Hero, About],
+  imports: [CommonModule, Hero, About, Connect],
   templateUrl: './home.html'
 })
 export class Home {
